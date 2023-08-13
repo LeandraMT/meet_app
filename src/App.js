adc73a63a3290ce886f32479c0b0f341ba855c84
LeandraMT <magantierleandra@gmail.com>
2023-08-14 01:32:26 +0200

    finalised adding the integration test and checking auth to access the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
 
 //Test Components
 import EventList from './components/EventList';
@@ -6,10 +10,31 @@ import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
 
 const App = () => {
+    const [events, setEvents] = useState([]);
+    const [currentNOE, setCurrentNOE] = useState(32);
+    const [allLocations, setAllLocations] = useState([]);
+    const [currentCity, setCurrentCity] = useState("See all cities");
+
+    const fetchData = async () => {
+        const allEvents = await getEvents();
+        const filteredEvents = currentCity === "See all cities" ?
+            allEvents : allEvents.filter(event => event.location === currentCity)
+
+        setEvents(filteredEvents.slice(0, currentNOE));
+        setAllLocations(extractLocations(allEvents));
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [currentCity]);
+
     return (
         <div className="App">
-            <CitySearch />
-            <EventList />
+            <CitySearch
+                allLocations={allLocations}
+                setCurrentCity={setCurrentCity}
+            />
+            <EventList allEvents={events} />
             <NumberOfEvents />
         </div>
     );
