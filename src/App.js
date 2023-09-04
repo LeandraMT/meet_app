90e9231fbed3ddfd8093ade42fd434f6ae37b870
LeandraMT <magantierleandra@gmail.com>
2023-09-05 01:34:59 +0200

    added styles to the charts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 
 //Test Components
+import EventGenresChart from './components/EventGenresChart';
 import CityEventsChart from './components/CityEventsChart';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
@@ -61,10 +62,15 @@ const App = () => {
                 setCurrentCity={setCurrentCity}
                 setInfoAlert={setInfoAlert}
             />
-            <CityEventsChart
-                allLocations={allLocations}
-                events={events}
-            />
+            <div className='charts-container'>
+                <EventGenresChart
+                    events={events}
+                />
+                <CityEventsChart
+                    allLocations={allLocations}
+                    events={events}
+                />
+            </div>
             <EventList
                 allEvents={events}
             />
