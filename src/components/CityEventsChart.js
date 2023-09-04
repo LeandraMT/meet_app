a7ba384584563c2edc745148a0643c56a480be9a
LeandraMT <magantierleandra@gmail.com>
2023-09-05 00:08:00 +0200

    added ResizeObserver error fix

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -19,7 +19,7 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length
-            const city = location.split(', ')[0]
+            const city = location.split((/, | - /))[0]
 
             return { count, city };
         })
@@ -32,14 +32,15 @@ const CityEventsChart = ({ allLocations, events }) => {
                 margin={{
                     top: 20,
                     right: 20,
-                    bottom: 20,
-                    left: 20,
+                    bottom: 60,
+                    left: -30,
                 }}
             >
                 <CartesianGrid />
                 <XAxis
                     type="category" dataKey="city" name="City"
-                    angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
+                    angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 20 }}
+                />
                 <YAxis type="number" dataKey="count" name="Number of Events" allowDecimals={false} />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
--- src/setupTests.js
+++ src/setupTests.js
@@ -15,3 +15,21 @@ console.error = (...args) => {
 }
 
 jest.setTimeout(30000);
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => (
+        {
+            observe: jest.fn(),
+            unobserve: jest.fn(),
+            disconnect: jest.fn(),
+        }
+    ));
+});
+
+afterEach(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
+});
